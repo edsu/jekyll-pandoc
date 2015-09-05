a1bd41fdcd243bb181aa0e2561a2f2a37108240b
Ed Summers <ehs@pobox.com>
2015-09-05 12:41:43 -0400

    try to make houndci happier

--- lib/jekyll-pandoc.rb
+++ lib/jekyll-pandoc.rb
@@ -1,19 +1,19 @@
 class Jekyll::Converters::Markdown::PandocProcessor
   DEFAULT_EXTENSIONS = []
-  DEFAULT_FORMAT = 'html5'
+  DEFAULT_FORMAT = "html5"
 
   def initialize(config)
-    require 'pandoc-ruby'
+    require "pandoc-ruby"
     @config = config
   rescue LoadError
-    STDERR.puts 'You are missing a library required for Pandoc. Please run:'
-    STDERR.puts ' $ [sudo] gem install pandoc-ruby'
+    STDERR.puts "You are missing a library required for Pandoc. Please run:"
+    STDERR.puts " $ [sudo] gem install pandoc-ruby"
     raise Jekyll::Errors::FatalException, "Missing dependency: pandoc-ruby"
   end
 
   def convert(content)
-    extensions = config_option('extensions', DEFAULT_EXTENSIONS)
-    format = config_option('format', DEFAULT_FORMAT)
+    extensions = config_option("extensions", DEFAULT_EXTENSIONS)
+    format = config_option("format", DEFAULT_FORMAT)
 
     content = PandocRuby.new(content, *extensions).send("to_#{format}")
     raise Jekyll::Errors::FatalException, "Conversion returned empty string" unless content.length > 0
@@ -21,8 +21,8 @@ class Jekyll::Converters::Markdown::PandocProcessor
   end
 
   def config_option(key, default=nil)
-    if @config['pandoc']
-      @config['pandoc'].fetch(key, default)
+    if @config["pandoc"]
+      @config["pandoc"].fetch(key, default)
     else
       default
     end
--- spec/pandoc_parser_spec.rb
+++ spec/pandoc_parser_spec.rb
@@ -1,32 +1,32 @@
-require 'spec_helper'
-require 'pandoc-ruby'
+require "spec_helper"
+require "pandoc-ruby"
 
 describe Jekyll::Converters::Markdown::PandocProcessor do
 
 
-  describe 'pandoc' do
-    config = {'markdown' => 'pandoc'}
+  describe "pandoc" do
+    config = { "markdown" => "pandoc" }
     config = Jekyll::Configuration::DEFAULTS.merge(config)
     subject { Jekyll::Converters::Markdown::PandocProcessor.new config }
-    it 'converts markdown' do 
+    it "converts markdown" do 
       markdown = "## This is a heading"
       html = "<h2 id=\"this-is-a-heading\">This is a heading</h2>\n"
       expect(subject.convert(markdown)).to eq(html)
     end
   end
 
-  describe 'bibliography' do
+  describe "bibliography" do
     config = {
-      'markdown' => 'pandoc',
-      'pandoc' => {
-        'extensions' => {
-          'bibliography' => 'spec/citations.bib'
+      "markdown" => "pandoc",
+      "pandoc" => {
+        "extensions" => {
+          "bibliography" => "spec/citations.bib"
 	}
       }
     }
     config = Jekyll::Configuration::DEFAULTS.merge(config)
     subject { Jekyll::Converters::Markdown::PandocProcessor.new config }
-    it 'converts markdown' do 
+    it "converts markdown" do 
       markdown = "Hmmm [@Bush:1945]"
       expect(subject.convert(markdown)).to match(/As We May Think/)
     end
